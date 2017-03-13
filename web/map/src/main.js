4e1742a998bba17d8b5167bf91682a88070fb9bb
Christopher Robert Philabaum <christopher@philabaum.me>
2017-03-12 20:36:50 -0700

    Added usage of data points instead of image points

--- web/map/src/main.js
+++ web/map/src/main.js
@@ -10,6 +10,13 @@ $(window).on("load", () => {
 function plotPoints(geoJSONLayer, data) {
     // Grab the points of every element.
     let geoDataPoints = data.map(elem => elem.loc);
+                ref1: binArray2FloatArray(point.ref1.$binary),
+                ref2: binArray2FloatArray(point.ref2.$binary)
     console.log(geoDataPoints);
     geoJSONLayer.addData(geoDataPoints);
+}
+
+function binArray2FloatArray(string) {
+    let byteArray = base64js.toByteArray(string);
+    return new Float32Array(byteArray.buffer);
 }

ba1d5e91563fe129b4bc49e26d97fbf169937eaf
Kelvin Rodriguez <kelvinrr@icloud.com>
2017-04-06 12:55:23 -0700

    a lot of stuff

---
+++ pygs/tests/test_database.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.mock import Mock
+from unittest.mock import MagicMock
+
+import ogr
+import pandas as pd
+from plio.io import io_gdal
+
+class TestEdge(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    def test_compute_image(self):
+        pass
